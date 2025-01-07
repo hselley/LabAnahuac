2f0b5daf4abd51d1886ba93754c0ce95d0770de4
Héctor Selley <hector.selley@gmail.com>
2025-01-07 16:32:04 -0600

    Versión inicial del sitio

---
+++ index.php
@@ -0,0 +1,20 @@
+<html lang="es-ES">
+<head>
+  <meta http-equiv="Content-Type" content="text/html;charset=utf-8" >
+  <meta charset="utf-8">
+</head>
+   <h3>
+   	<ul>
+<?
+ //    	  <li> <a href="taller-bootcamp/index.html"> Construcción y programación de un dispositivo de ayuda para personas con discapacidad del habla </a>
+//<h3> Talleres de ISTI para la Semana de Orientación Vocacional </h3>
+?>
+	<h3> Talleres de ISTI para eventos de Orientación Vocacional </h3>
+	<li><a href='Python'>Programación en Python</a><br></li>
+	<li><a href='Acuario'>Acuario: Mi primera Base de Datos</a><br></li>
+	<li><a href='ISTI'>Información de la carrera de ISTI - Ingeniería en Sistemas y Tecnologías de Información</a><br></li>
+	<li><a href='arduino'>Taller de Arduino</a><br></li>
+        </ul>
+  </h3>
+</body>
+</html>
